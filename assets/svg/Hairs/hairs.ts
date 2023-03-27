e554bc18e363ba5b22f6f0e745cc2752f134e5dd
Tristan | Tribot17 <tribot17@gmail.com>
2023-03-28 01:40:19 +0200

    colorMap

--- assets/svg/Hairs/hairs.ts
+++ assets/svg/Hairs/hairs.ts
@@ -1,4 +1,6 @@
-export const hairs = {
+import { ColorMap } from "../../../interfaces/interfaces";
+
+export const hairs: ColorMap = {
   "1": (hue: string) => {
     return `<svg 
     viewBox="0 0 878 279" 
