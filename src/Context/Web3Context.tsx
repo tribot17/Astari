caf8ce4fa0bd235c2e6cfee3a78acce779697326
Tristan | Tribot17 <tribot17@gmail.com>
2023-03-28 01:41:01 +0200

    create mintInstace and ContractInstance

--- src/Context/Web3Context.tsx
+++ src/Context/Web3Context.tsx
@@ -1,20 +1,31 @@
 import { createContext, useEffect, useState } from "react";
 import Web3 from "web3";
-export const Web3Context = createContext({
-  web3: {},
-  walletAddress: "",
-  isLoading: false,
-  getData: () => {},
-});
+import contractABI from "../../ABI/FirstWeaponABI.json";
+interface Web3ContextData {
+  web3: any;
+  walletAddress: string | undefined;
+  mintContract: any;
+  isLoading: boolean;
+  getData: () => Promise<any>;
+}
 
 interface Web3Provider {
   children: any;
 }
 
+export const Web3Context = createContext<Web3ContextData>({
+  web3: {},
+  walletAddress: undefined,
+  mintContract: {},
+  isLoading: false,
+  getData: async () => {},
+});
+
 export const Web3ContextProvider: React.FC<Web3Provider> = ({ children }) => {
   const [web3, setWeb3] = useState<any>("");
   const [walletAddress, setWalletAddress] = useState<string>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [mintContract, setMintContract] = useState<any>();
 
   const getData = async () => {
     return new Promise(async (res, rej) => {
@@ -37,12 +48,20 @@ export const Web3ContextProvider: React.FC<Web3Provider> = ({ children }) => {
       }
 
       setWeb3(web3Instance);
+      setMintContract(
+        new web3Instance.eth.Contract(
+          contractABI,
+          "0xD2310118F25997b21B724B8262aaAf3d51521CdE"
+        )
+      );
       setIsLoading(false);
     });
   };
 
   return (
-    <Web3Context.Provider value={{ web3, walletAddress, isLoading, getData }}>
+    <Web3Context.Provider
+      value={{ web3, walletAddress, mintContract, isLoading, getData }}
+    >
       {children}
     </Web3Context.Provider>
   );
--- src/Pages/MintPage/index.tsx
+++ src/Pages/MintPage/index.tsx
@@ -1,20 +1,33 @@
 import Web3 from "web3";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import contractABI from "../../../ABI/FirstWeaponABI.json";
 import styles from "./MintPage.module.scss";
+import Web3Context from "../../Context/Web3Context";
 
 const index = () => {
+  const { web3, walletAddress, mintContract, isLoading, getData } =
+    useContext(Web3Context);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleMint = async () => {
+    if (mintContract) {
+      const mint = await mintContract.methods
+        .mint()
+        .send({ from: walletAddress });
+    }
+  };
+
   return (
     <div className={styles.mint_page}>
-      {/* <button onClick={() => handleConnect()}>connect</button> */}
-      {/* <button onClick={() => seeContract()}>See contract</button> */}
-      {/* <button onClick={() => handleMint()}>Mint</button> */}
       <h1>MINT YOUR FIRST WEAPON</h1>
       <div className={styles.mint_container}>
         <div className={styles.NFT_images}></div>
-        {/* <button onClick={() => handleMint()}>
+        <button onClick={() => handleMint()}>
           <p>MINT</p>
-        </button> */}
+        </button>
       </div>
     </div>
   );
