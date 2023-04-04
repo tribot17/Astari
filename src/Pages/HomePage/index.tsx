c435b4683ce4a54c43e580853fa2bf21351070f0
Tristan | Tribot17 <tribot17@gmail.com>
2023-04-04 17:52:04 +0200

    update import to root

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -6,20 +6,20 @@ const HomePage = () => {
 
   return (
     <div className={styles.homePage}>
-      <img src="../../../assets/stars.png" id={styles.stars} />
-      <img src="../../../assets/earth.png" id={styles.earth} />
+      <img src="./public/stars.png" id={styles.stars} />
+      <img src="./public/earth.png" id={styles.earth} />
       <div className={styles.logo_button}>
-        <img src="../../../assets/logoAll.png" id={styles.logoAll} />
+        <img src="./public/logoAll.png" id={styles.logoAll} />
         <button>
           <p>EXPLORE</p>
         </button>
       </div>
-      <img src="../../../assets/sun.png" id={styles.sun} />
-      <img src="../../../assets/planet16.png" id={styles.planet16} />
-      <img src="../../../assets/mars.png" id={styles.mars} />
-      <img src="../../../assets/jupiter.png" id={styles.jupiter} />
-      <img src="../../../assets/planet.png" id={styles.planet} />
-      <img src="../../../assets/moon.png" id={styles.moon} />
+      <img src="./public/sun.png" id={styles.sun} />
+      <img src="./public/planet16.png" id={styles.planet16} />
+      <img src="./public/mars.png" id={styles.mars} />
+      <img src="./public/jupiter.png" id={styles.jupiter} />
+      <img src="./public/planet.png" id={styles.planet} />
+      <img src="./public/moon.png" id={styles.moon} />
     </div>
   );
 };
